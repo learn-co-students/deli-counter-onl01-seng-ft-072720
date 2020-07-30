b66b080c708f25326ace04c883cfec983db5eaad
George Talnagi <gtalnagi@yahoo.com>
2020-07-30 21:24:11 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+def line(array)
+  first_in_line = []
+  if array.empty?
+    puts "The line is currently empty"
+  else
+    array.each_with_index do |name, index|
+      new_array.push("#{index + 1}. #{name}")
+    end
+    new_array.unshift("The line is currently: ")
+  end
+end
+
+def take_a_number
+end
+
+def now_serving
+end
