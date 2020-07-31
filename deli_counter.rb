ca3050f3f9f6961ca9951d9f5da1d1d61221d340
George Talnagi <gtalnagi@yahoo.com>
2020-07-31 17:21:50 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,17 +1,27 @@
-def line(array)
-  first_in_line = []
-  if array.empty?
-    puts "The line is currently empty"
+katz_deli = []
+
+def line(katz_deli)
+  phrase = "The line is currently:"
+  if katz_deli.empty?
+    puts "The line is currently empty."
   else
-    array.each_with_index do |name, index|
-      new_array.push("#{index + 1}. #{name}")
+    katz_deli.each_with_index do |name, index|
+      phrase << " #{index + 1}. #{name}"
     end
-    new_array.unshift("The line is currently: ")
+  puts phrase
   end
 end
 
-def take_a_number
+def take_a_number(katz_deli, name)
+  katz_deli << name
+puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
-def now_serving
+def now_serving(katz_deli)
+  if katz_deli.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli.first}."
+    katz_deli.shift
+  end
 end
